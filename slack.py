f339a31ae7d13d2cfecc6138d0c53ab43c4dffeb
Mounika Perla <mounikaperla@lorangtechnologies.com>
2023-01-11 18:47:22 +0530

    slack is comitted

---
+++ slack.py
@@ -0,0 +1,30 @@
+import requests
+import sys
+import getopt
+
+def send_slack_message(message):
+    payload ='{"text":"%s"}' % message
+    response =requests.post('https://hooks.slack.com/services/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',data=payload)
+
+    print(response.text)
+
+def main(argv):
+
+    message = ' '
+
+    try:opts,args=getopt.getopt(argv,"hm:",["message="])
+
+    except getopt.GetoptError:
+        print('SlackMessage.py -m <message>')
+        sys.exit(2)
+    if len(opts) == 0:
+        message ="HELLO,WORLD!"
+    for opt,arg in opts:
+        if opt=='-h':
+            print('SlackMessage.py -m <message>')
+            sys.exit()
+        elif opt in("-m","--message"):
+            message =arg
+    send_slack_message(message)
+if __name__ == "__main__":
+    main(sys.argv[1:])
